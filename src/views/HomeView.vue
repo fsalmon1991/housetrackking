8ba76a20f5b69a97e6a65b28a6dbe5b3e41ea6f0
Francisco Salmon Moret <franciscosalmonmoret@Franciscos-MBP.attlocal.net>
2022-10-04 19:35:44 -0400

    first commit on jratlas proyect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search/Result-->
+    <div
+      class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">Species Tracker</h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for species in the Gardens of the Queen National Park Area"
+          />
+          <i
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-solid fa-chevron-right"
+          ></i>
+        </div>
+      </div>
+      <!-- Species Info-->
+      <SpeciesInfo />
+    </div>
+    <!-- Map -->
+    <div id="map" class="h-full z-10"></div>
   </div>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SpeciesInfo from "../components/SpeciesInfo.vue";
+import { onMounted } from "vue";
+import leaflet from "leaflet";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+let map;
+onMounted(() => {
+  map = leaflet.map("map").setView([20.85788702677816, -78.97071006615306], 11);
+  leaflet
+    .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      maxZoom: 13,
+      minZoom: 11,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoiZnNhbG1vbjE5OTEiLCJhIjoiY2w4dXRlemIyMDJtcDQwbnU0aG9ib3lobyJ9.8wpifPoqm2o9KCDLu6mmlQ",
+    })
+    .addTo(map);
+});
 </script>
